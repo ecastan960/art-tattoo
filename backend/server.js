1e56ad2745afecf9cee226d2101f24d3f27c923f
dev-ecastan <esteban.castano@devsavant.ai>
2023-01-25 03:51:20 -0500

    [02] Update Backend Setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import path from 'path';
+// import uploadRoutes from './routes/uploadRoutes.js';
 
 
 dotenv.config();
@@ -11,9 +16,36 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('API is running....');
+  res.send('API is running...');
 });
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+// app.use('/api/upload', uploadRoutes);
+
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req,res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req,res) => {
+    res.send('API is running....');
+  })
+}
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
